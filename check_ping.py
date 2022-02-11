79c94226b4046bcbfa1d8fdc5f76dbf5f762d855
Tshuen Hau <tshuenhau@gmail.com>
2022-02-12 00:50:30 +0800

    check_ping pings 3 times

--- check_ping.py
+++ check_ping.py
@@ -2,4 +2,10 @@ import os
 
 def check_ping(hostname):
 	response = os.system("ping -c 1 " + hostname)
+	if(response == 1):
+		response = os.system("ping -c 1 " + hostname)
+		if(response == 1):
+			response = os.system("ping -c 1 " + hostname)
+		
+	
 	return response
