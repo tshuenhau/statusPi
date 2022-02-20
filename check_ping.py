a159ace50797b8c679db4f2aff9cd8c5a1051d71
Tshuen Hau <tshuenhau@gmail.com>
2022-02-21 00:41:30 +0800

    added delay in between each ping

--- check_ping.py
+++ check_ping.py
@@ -1,10 +1,12 @@
 import os
-
+import time
 def check_ping(hostname):
 	response = os.system("ping -c 1 " + hostname)
 	if(response == 1):
+		time.sleep(5)	
 		response = os.system("ping -c 1 " + hostname)
 		if(response == 1):
+			time.sleep(5)
 			response = os.system("ping -c 1 " + hostname)
 		
 	
